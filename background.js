888c7b9bc5abfb59be8ed49b4e0be7f471855f1c
alec-deng <alexdeng901@gmail.com>
2025-02-23 02:12:50 -0500

    update background using switch

--- background.js
+++ background.js
@@ -8,15 +8,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("Message received:", message);
   console.log("Sender details:", sender);
 
-  if (message.action === "fetchData") {
-    fetchList(message.userName, message.type)
-      .then((data) => {
-        sendResponse(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        sendResponse({ error: "Failed to fetch data" });
-      });
-    return true;
+  switch (message.action) {
+    case "fetchData":
+      fetchList(message.userName, message.type)
+        .then((data) => {
+          sendResponse(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          sendResponse({ error: "Failed to fetch data" });
+        });
   }
+
+  // Keep listener active to wait for async response
+  return true;
 });
